309ee19f91e0d14d9e40d4e66ddf635a49c1bc3c
Timothy Mak <tshmak@hku.hk>
2018-01-24 11:48:11 +0800

    allows ...

--- R/print.plinkout.R
+++ R/print.plinkout.R
@@ -1,5 +1,5 @@
-print.plinkout <- function(plinkout) {
+print.plinkout <- function(plinkout, ...) {
   ### Function to give the plink output file stub without the attributes
   attributes(plinkout) <- NULL
-  print(plinkout)
+  print(plinkout, ...)
 }
