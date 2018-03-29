2b8557b4e854f2a9a1d689dec3510102716b6528
Timothy Mak <tshmak@hku.hk>
2018-03-29 13:12:02 +0800

    For new Linux machine

--- R/options.R
+++ R/options.R
@@ -1,8 +1,32 @@
 if(Sys.info()["sysname"] == "Windows") {
   options(Rplink.plink.exec="D:/PLINK/plink.exe")
 } else {
-  options(Rplink.plink.exec="/home/tshmak/software/plink/v1.90b3.44/plink")
-  options(Rplink.plink2.exec="/home2/groups/pcsham/projects/UKB/plink2/plink2")
+  # if(is.null(getOption("Rplink.plink.exec"))) {
+  #   tryget <- Sys.getenv("plink")
+  #   if(tryget == "") {
+  #     message(paste("Cannot find plink executable. Please specify in 'Rplink.plink.exec',",
+  #                "or specify a 'plink' variable in .bashrc"))
+  #   } else {
+  #     options(Rplink.plink.exec=tryget)
+  #   }
+  # }
+  #
+  # if(is.null(getOption("Rplink.plink2.exec"))) {
+  #   tryget <- Sys.getenv("plink2")
+  #   if(tryget == "") {
+  #     message(paste("Cannot find plink2 executable. Please specify in 'Rplink.plink2.exec',",
+  #                "or specify a 'plink2' variable in .bashrc"))
+  #   } else {
+  #     options(Rplink.plink.exec=tryget)
+  #   }
+  # }
+  if(Sys.info()["nodename"] == "GRC170") {
+    options(Rplink.plink.exec="/home/tshmak/software/plink/v1.90b5.2/plink")
+    # options(Rplink.plink2.exec="/home2/groups/pcsham/projects/UKB/plink2/plink2")
+  } else {
+    options(Rplink.plink.exec="/home/tshmak/software/plink/v1.90b4.4/plink")
+    options(Rplink.plink2.exec="/home2/groups/pcsham/projects/UKB/plink2/plink2")
+  }
 }
 
 if("data.table" %in% .packages(all.available=T)) {
--- R/run.plink.R
+++ R/run.plink.R
@@ -3,8 +3,10 @@ run.plink <- function(cmd, plink2=FALSE) {
   # args <- list(...)
   if(plink2) {
     plink.exec <- getOption("Rplink.plink2.exec")
+    if(is.null(plink.exec)) stop("Rplink.plink2.exec not defined in options.")
   } else {
     plink.exec <- getOption("Rplink.plink.exec")
+    if(is.null(plink.exec)) stop("Rplink.plink.exec not defined in options.")
   }
 
   if(Sys.info()["sysname"] == "Windows") {
